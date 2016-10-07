11a83592c2b7bcc0af8ba55475762bd2ad52683c
Yassine AZZOUT <92bondstreet@gmail.com>
2016-10-07 23:02:54 +0200

    feat(box): count remaining characters

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
 
 class TweetBox extends Component {
+  constructor (){
+    super();
+    this.state = {
+      'content': ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  /**
+   * Content tweet change handler
+   *
+   * @param {Event} event
+   */
+  handleChange (event) {
+    this.setState({'content': event.target.value});
+  }
+
+  /**
+   * Render tweet box
+   */
   render () {
+    const {content} = this.state;
+    const count = 140 - content.length;
+    const disabled = ! content || count < 0;
+
     return (
       <div className="well clearfix">
-        <textarea className="form-control"></textarea>
+        <textarea className="form-control" onChange={this.handleChange}></textarea>
         <br/>
-        <button className="btn btn-primary pull-right" disabled>Tweet</button>
+        <span>{count}</span>
+        <button className="btn btn-primary pull-right" disabled={disabled}>Tweet</button>
       </div>
     );
   }
