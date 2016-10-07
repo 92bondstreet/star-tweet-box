419ce2bb878eb54b77905ba38c59a030c59bea51
Yassine AZZOUT <92bondstreet@gmail.com>
2016-10-07 23:17:42 +0200

    feat(box): add photo

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -4,9 +4,11 @@ class TweetBox extends Component {
   constructor (){
     super();
     this.state = {
-      'content': ''
+      'content': '',
+      'photo': false
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleContent = this.handleContent.bind(this);
+    this.handlePhoto = this.handlePhoto.bind(this);
   }
 
   /**
@@ -14,24 +16,30 @@ class TweetBox extends Component {
    *
    * @param {Event} event
    */
-  handleChange (event) {
+  handleContent (event) {
     this.setState({'content': event.target.value});
+  }
+
+  handlePhoto () {
+    this.setState({'photo': ! this.state.photo});
   }
 
   /**
    * Render tweet box
    */
   render () {
-    const {content} = this.state;
-    const count = 140 - content.length;
-    const disabled = ! content || count < 0;
+    const {content, photo} = this.state;
+    const count = 140 - content.length - (23 * photo | 0);
+    const disabled = count === 140 || count < 0;
+    const add = this.state.photo ? '✓ Photo Added' : 'Add Photo';
 
     return (
       <div className="well clearfix">
-        <textarea className="form-control" onChange={this.handleChange}></textarea>
+        <textarea className="form-control" onChange={this.handleContent}></textarea>
         <br/>
         <span>{count}</span>
         <button className="btn btn-primary pull-right" disabled={disabled}>Tweet</button>
+        <button className="btn btn-default pull-right" onClick={this.handlePhoto}>{add}</button>
       </div>
     );
   }
